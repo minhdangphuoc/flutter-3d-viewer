d830946fb01de49ee16935ae7fa5cfd23ad04442
Minh Dang <minhdang@OMEN-Laptop-15-en0xxx-90559928.localdomain>
2021-06-06 05:11:45 +0300

    First Build

---
+++ src/three_d_viewer/lib/main.dart
@@ -0,0 +1,34 @@
+import 'custom_navigation_drawer.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Navigation Drawer Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      drawer: CollapsingNavigationDrawer(),
+      body: Stack(
+        children: <Widget>[
+          Container(color: selectedColor,),
+          CollapsingNavigationDrawer()
+        ],
+      )
+    );
+  }
+}
---
+++ src/three_d_viewer/lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+TextStyle listTitleDefaultTextStyle =  
+    TextStyle(color: Color(0xFFE1F6F4), fontSize: 20.0, fontWeight: FontWeight.w600);
+TextStyle listTitleSelectedTextStyle =
+    TextStyle(color: Color(0xFFE1F6F4), fontFamily: 'WorkSans', fontSize: 18.0, fontWeight: FontWeight.w600);
+
+Color selectedColor = Color(0xFFE1F6F4);
+Color drawerBackgroundColor = Color(0xFF7DB9B3);
